8583cd414e1773a812d454d2d4c5e8d18c9d1a1a
Gulferamus <gulferamus.fc@gmail.com>
2021-05-12 00:09:32 +0200

    aggiunto street spawning

--- Assets/scripts/LevelManager.cs
+++ Assets/scripts/LevelManager.cs
@@ -5,23 +5,43 @@ using UnityEngine;
 public class LevelManager : MonoBehaviour
 {
     [SerializeField]
-    private GameObject street;
-    [SerializeField]
     private float streetSpeed = 9;
     [SerializeField]
     private short maxStreets = 3;
+    [SerializeField]
+    private GameObject streetPrefab;
 
-    public GameObject[] streets { private get; set; }
+    private GameObject[] streets;
+    private Renderer streetRenderer;
+    private float streetLength;
 
-    private void Awaken()
+
+    private void Awake()
     {
-        streets = new GameObject[maxStreets];
         streetSpeed *= -Time.deltaTime/10;
+
+        streets = new GameObject[maxStreets];
+        streets[0] = GameObject.Find("Street");
+
+        streetRenderer = streetPrefab.GetComponent<MeshRenderer>();
+        streetLength = streetRenderer.bounds.size.z;
     }
 
     // Update is called once per frame
     void Update()
     {
-        street.transform.Translate(0, 0, streetSpeed);
+        // TODO: considerare nell'instantiate che le strade si muovono, e quindi il punto di spawn è variabile
+        for (int i = 0; i < streets.Length; i++)
+        {
+            if (streets[i] == null)
+            {
+                streets[i] = Instantiate(streetPrefab, new Vector3(0, 0, i*streetLength + 25), Quaternion.identity);
+            }
+        }
+
+        foreach (GameObject street in streets)
+        {
+            street.transform.Translate(0, 0, streetSpeed);
+        }
     }
 }
