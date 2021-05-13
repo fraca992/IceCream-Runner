beba444d6e45f623b01d2e9c20eda44fcd0e6e0a
Gulferamus <gulferamus.fc@gmail.com>
2021-05-13 23:55:47 +0200

    sostituito l'asfalto della strada con un cubo al posto del piano: ha tolto quel gap dovuto allo spostamento SENZA  ricorrere all'oggetto vuoto padre

--- Assets/scripts/LevelManager.cs
+++ Assets/scripts/LevelManager.cs
@@ -10,8 +10,6 @@ public class LevelManager : MonoBehaviour
     private short maxStreets = 3;
     [SerializeField]
     private GameObject streetPrefab;
-    [SerializeField]
-    private GameObject level;
 
     private GameObject[] streets;
     private Renderer streetRenderer;
@@ -22,24 +20,24 @@ public class LevelManager : MonoBehaviour
         streets = new GameObject[maxStreets];
         streets[0] = GameObject.Find("Street");
 
-        streetRenderer = streetPrefab.GetComponent<MeshRenderer>();
+        streetRenderer = streetPrefab.GetComponentInChildren<MeshRenderer>();
         streetLength = streetRenderer.bounds.size.z;
     }
 
     void Update()
     {
-        float streetOverhead = streetLength/2f;
         // TODO: considerare nell'instantiate che le strade si muovono, e quindi il punto di spawn è variabile
         for (int i = 0; i < streets.Length; i++)
         {
             if (streets[i] == null)
             {
-                streets[i] = Instantiate(streetPrefab, new Vector3(0, 0, streetOverhead + (i * streetLength)), Quaternion.identity, level.transform);
+                streets[i] = Instantiate(streetPrefab, new Vector3(0, 0, (i * streetLength)), Quaternion.identity);
             }
         }
 
-
-            level.transform.Translate(0, 0, - streetSpeed * Time.deltaTime);
-
+        foreach (GameObject street in streets)
+        {
+            street.transform.Translate(0, 0, -streetSpeed * Time.deltaTime);
+        }
     }
 }
