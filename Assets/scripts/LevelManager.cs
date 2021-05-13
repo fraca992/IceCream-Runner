c8b18e512b224e90ddc0c68e6dfef3a2d8df0204
Gulferamus <gulferamus.fc@gmail.com>
2021-05-13 23:21:03 +0200

    spawnate le strade sotto un oggetto parente vuoto, e funziona... ma così il parente scorre e va lontanissimo = problemi se il livello va troppo a lungo

--- Assets/scripts/LevelManager.cs
+++ Assets/scripts/LevelManager.cs
@@ -10,6 +10,8 @@ public class LevelManager : MonoBehaviour
     private short maxStreets = 3;
     [SerializeField]
     private GameObject streetPrefab;
+    [SerializeField]
+    private GameObject level;
 
     private GameObject[] streets;
     private Renderer streetRenderer;
@@ -24,21 +26,20 @@ public class LevelManager : MonoBehaviour
         streetLength = streetRenderer.bounds.size.z;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        float streetOverhead = streetLength/2f;
         // TODO: considerare nell'instantiate che le strade si muovono, e quindi il punto di spawn è variabile
         for (int i = 0; i < streets.Length; i++)
         {
             if (streets[i] == null)
             {
-                streets[i] = Instantiate(streetPrefab, new Vector3(0, 0, i*streetLength + 25), Quaternion.identity);
+                streets[i] = Instantiate(streetPrefab, new Vector3(0, 0, streetOverhead + (i * streetLength)), Quaternion.identity, level.transform);
             }
         }
 
-        foreach (GameObject street in streets)
-        {
-            street.transform.Translate(0, 0, - streetSpeed * Time.deltaTime);
-        }
+
+            level.transform.Translate(0, 0, - streetSpeed * Time.deltaTime);
+
     }
 }
