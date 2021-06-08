f8561fa305981ac30b77d44a085a485c50889ecd
Gulferamus <gulferamus.fc@gmail.com>
2021-06-08 23:55:25 +0200

    Started working on the ObjectSpawner script [WIP]

---
+++ Assets/scripts/Common/ObjectSpawner.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace Common
+{
+    public class ObjectSpawner : Object
+    {
+        private GameObject[] obstacles;
+
+        public ObjectSpawner()
+        {
+            // Loading all obstacle prefabs
+            obstacles = Resources.LoadAll<GameObject>("Obstacles");
+        }
+
+        void SpawnObstacles(int budget, Vector3[] sidewalkCoords)
+        {
+            // TODO: buy obstacles using budget
+            int buyIndex;
+            List<GameObject> boughtItems = new List<GameObject>();
+
+            while (budget > 0)
+            {
+                buyIndex = Random.Range(0, obstacles.Length);
+                boughtItems.Add(obstacles[buyIndex]);
+                //budget -= obstacles[buyIndex].GetComponent<>();
+            }
+            
+            
+
+            // TODO: choose a random obstacle
+            
+            // TODO: choose a random cell using distributed probability
+
+            // TODO: update cell points
+
+        }
+    } 
+}
