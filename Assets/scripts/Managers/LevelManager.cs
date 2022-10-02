1b94c5809de952f498fb1fa7aa5a107fd999fc2d
Gulferamus <gulferamus.fc@gmail.com>
2022-10-02 12:27:14 +0300

    solved street spwaening issue

--- Assets/scripts/Managers/LevelManager.cs
+++ Assets/scripts/Managers/LevelManager.cs
@@ -14,7 +14,7 @@ public class LevelManager : MonoBehaviour
 
     #region Street Variables
     [SerializeField]
-    private int stackSize = 7;
+    private int stackSize = 10;
     private StreetStack lvl1Stack;
     private GameObject nextStreet;
     private string streetPrefabPath = "Streets/Street";
@@ -33,34 +33,33 @@ public class LevelManager : MonoBehaviour
     {
         player = GameObject.Find("Player");
 
-        lvl1Stack = new StreetStack(stackSize);
+        lvl1Stack = new StreetStack(stackSize, streetPrefabPath);
         for (int i = 0; i < stackSize; i++)
         {
-            streetSpawnCoordinates = GetNextStreetCoordinates(lvl1Stack);
-            nextStreet = SpawnStreet(streetPrefabPath, streetSpawnCoordinates, streetbudget, Tools.GetNextValue(), streetXCellNum, streetZCellNum, streetCellPoints);
-            lvl1Stack.InsertStreetIntoStack(nextStreet);
+            lvl1Stack.SpawnStreet(streetbudget, streetXCellNum, streetZCellNum, streetCellPoints);
         }
     }
 
     void Update()
     {
-        // Moving the streets in the stack
-        streetMovement.z -= levelSpeed * Time.deltaTime;
-        foreach (GameObject strt in lvl1Stack.streets)
-        {
-            strt.GetComponent<Rigidbody>().MovePosition(streetMovement);
-        }
-
-        //TODO: spawn street continuously when 1 full street is behind the player
+        // Spawn street continuously when 1 full street is behind the player
         float lastStreetPositionZ = lvl1Stack.streets[0].transform.position.z;
         float playerPosition = player.transform.position.z;
-        float distanceBuffer = Tools.GetSize(lvl1Stack.streets[0], 'z');
+        float distanceBuffer = Tools.GetSize(lvl1Stack.streets[0], 'z')*2;
 
         if (lastStreetPositionZ + distanceBuffer < playerPosition)
         {
-            streetSpawnCoordinates = GetNextStreetCoordinates(lvl1Stack); // è copiato dallo spawn iniziale in Awakw. non mi piace, idealmente dovrebbe essere una funzione (o integra le 3 linee di codice in SpawnStreet direttamente)
-            nextStreet = SpawnStreet(streetPrefabPath, streetSpawnCoordinates, streetbudget, Tools.GetNextValue(), streetXCellNum, streetZCellNum, streetCellPoints);
-            lvl1Stack.InsertStreetIntoStack(nextStreet);
+            lvl1Stack.SpawnStreet(streetbudget, streetXCellNum, streetZCellNum, streetCellPoints);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // Moving the streets in the stack
+        streetMovement.z = -levelSpeed; //* Time.deltaTime;
+        foreach (GameObject strt in lvl1Stack.streets)
+        {
+            strt.GetComponent<Rigidbody>().MovePosition(strt.transform.position + streetMovement);
         }
     }
 
@@ -68,37 +67,46 @@ public class LevelManager : MonoBehaviour
     {
         public int StackSize { get; private set; }
         public int StackCount { get { return streets.Count; } }
+        public string Path { private get; set; }
 
         public List<GameObject> streets = new List<GameObject>();
 
-        public StreetStack(int size = 5)
+        public StreetStack(int size = 5, string path = "")
         {
             StackSize = size;
+            Path = path;
         }
 
-        internal void InsertStreetIntoStack (GameObject street)
+        public GameObject SpawnStreet(int budget, int xCellNum, int zCellNum, int cellPoints)
+        {
+            Vector3 spawnCoordinates = GetNextStreetCoordinates();
+
+            GameObject nextStreet = Instantiate(Resources.Load<GameObject>(Path), spawnCoordinates, Quaternion.identity);
+            nextStreet.GetComponent<StreetProperties>().InitializeStreetProperties(budget, Tools.GetNextValue(), xCellNum, zCellNum, cellPoints);
+            InsertStreetIntoStack(nextStreet);
+
+            return nextStreet;
+        }
+
+        private Vector3 GetNextStreetCoordinates()
+        {
+            Vector3 nextCoords = Vector3.zero;
+            float streetLength = StackCount == 0 ? 0 : Tools.GetSize(streets[StackCount - 1], 'z');
+
+            nextCoords.z = StackCount == 0 ? 0 : streets[StackCount - 1].transform.position.z + streetLength;
+
+            return nextCoords;
+        }
+        private void InsertStreetIntoStack(GameObject street)
         {
             // Before inserting a new street segment into the stack, we remove the oldest one (index = 0), if it would exceed the stack's max size
-            if(streets.Count == StackSize) streets.RemoveAt(0); // REVIEW may need a destroy game object first?
+            if (streets.Count == StackSize)
+            {
+                GameObject.Destroy(streets[0]);
+                streets.RemoveAt(0);
+            }
+
             streets.Add(street);
         }
-    }
-
-    private GameObject SpawnStreet(string path, Vector3 spawnCoordinates, int budget, int id, int xCellNum, int zCellNum, int cellPoints)
-    {
-        GameObject nextStreet = Instantiate(Resources.Load<GameObject>(path), spawnCoordinates, Quaternion.identity);
-        nextStreet.GetComponent<StreetProperties>().InitializeStreetProperties(budget, id, xCellNum, zCellNum, cellPoints);
-
-        return nextStreet;
-    }
-
-    private Vector3 GetNextStreetCoordinates(StreetStack stack)
-    {
-        Vector3 nextCoords = Vector3.zero;
-        float streetLength = stack.StackCount == 0 ? 0 : Tools.GetSize(stack.streets[lvl1Stack.StackCount - 1], 'z');
-
-        nextCoords.z = stack.StackCount == 0 ? 0 : stack.streets[stack.StackCount - 1].transform.position.z + streetLength;
-
-        return nextCoords;
     }
 }
