1fc0190b2c41f05b697fd96208effd668cefe902
FrancescoC <francesco.capirci@gmail.com>
2023-02-02 23:19:32 +0200

    WIP - Refactoring

--- Assets/scripts/Properties/CellProperties.cs
+++ Assets/scripts/Properties/CellProperties.cs
@@ -1,14 +1,14 @@
 using UnityEngine;
 
 
-public class Cell
+public class CellProperties
 {
     public bool isOccupied { get; set; }
     public Vector3 Coordinates { get; set; }
     public int Id { get; private set; }
 
     // Constructor
-    public Cell(int id = 0, Vector3? c = null)
+    public CellProperties(int id = 0, Vector3? c = null)
     {
         isOccupied = false;
         Id = id;
--- Assets/scripts/Properties/StreetPieceProperties.cs
+++ Assets/scripts/Properties/StreetPieceProperties.cs
@@ -2,23 +2,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using Common;
 
-public class GroundProperties : MonoBehaviour
+public class StreetPieceProperties : MonoBehaviour
 {
     #region Variables
     public int Budget { get; private set; }
     public int Id { get; private set; }
+    public float Length { get; private set; }
     public float SidewalkWidth { get; private set; }
-    public float SidewalkLength { get; private set; }
     public float SidewalkHeight { get; private set; }
+    public float RoadWidth { get; private set; }
+    public float RoadHeight { get; private set; }
     #endregion
 
     // Constructor
-    public void InitializeGroundProperties (int budget, int id)
+    public void InitializeGroundProperties (int id)
     {
-        Budget = budget;
         Id = id;
+
+        Length = Tools.GetSize(this.transform.gameObject, 'z');
         SidewalkWidth = Tools.GetSize(this.transform.GetChild(1).gameObject, 'x');
-        SidewalkLength = Tools.GetSize(this.transform.GetChild(1).gameObject, 'z');
         SidewalkHeight = Tools.GetSize(this.transform.GetChild(1).gameObject, 'y');
+        RoadWidth = Tools.GetSize(this.transform.GetChild(0).gameObject, 'x');
+        RoadHeight = Tools.GetSize(this.transform.GetChild(0).gameObject, 'y');
     }
 }
