f67b181b1480fe4f1ba9af295eaec4599b201e76
FrancescoC <francesco.capirci@gmail.com>
2023-02-19 12:15:35 +0200

    commit before branching

--- Assets/scripts/Managers/SegmentManager.cs
+++ Assets/scripts/Managers/SegmentManager.cs
@@ -58,7 +58,6 @@ public class SegmentManager
             newCells.Add(new CellProperties(newCellID));
         }
 
-
         // Spawning obstacles
         obstacleSpawner.FillItemList(maxObstacles, totalBudget);
         List<ObstacleProperties> newObstacles = obstacleSpawner.PlaceObstacles(newCells);
@@ -98,43 +97,5 @@ public class SegmentManager
         }
 
         segmentStack.Add(seg);
-    }
-
-    // Computes an updated position for the cells // TODO: adatta ad essere usat qua
-    private List<CellProperties> GetUpdatedCellCoordinates(GameObject streetPiece, List<CellProperties> cells, int xCellNumber, float cellSize)
-    {
-        // compute cell coordinates
-        int zIndex = 0;
-        int xIndex = 0;
-        Vector3 cellCoordinatesDelta = new Vector3(0f, 0f, 0f);
-        StreetPieceProperties strtPcProperties = streetPiece.GetComponent<StreetPieceProperties>();
-
-        for (int i = 0; i < cells.Count / 2; i++)
-        {
-            zIndex = i / xCellNumber;
-            xIndex = i - zIndex * xCellNumber;
-
-            cellCoordinatesDelta.x = (2 * xIndex + 1) * cellSize / 2f - strtPcProperties.SidewalkWidth / 2f;
-            cellCoordinatesDelta.y = strtPcProperties.SidewalkHeight;
-            cellCoordinatesDelta.z = strtPcProperties.Length / 2f - (2 * zIndex + 1) * cellSize / 2f;
-
-            // the final cell coordinates
-            cells[i].Coordinates = streetPiece.transform.GetChild(1).position + cellCoordinatesDelta;
-        }
-        for (int i = cells.Count / 2; i < cells.Count; i++)
-        {
-            int ii = i - cells.Count / 2;
-            zIndex = ii / xCellNumber;
-            xIndex = ii - zIndex * xCellNumber;
-
-            cellCoordinatesDelta.x = (2 * xIndex + 1) * cellSize / 2f - strtPcProperties.SidewalkWidth / 2f;
-            cellCoordinatesDelta.y = strtPcProperties.SidewalkHeight;
-            cellCoordinatesDelta.z = strtPcProperties.Length / 2f - (2 * zIndex + 1) * cellSize / 2f;
-
-            // the final cell coordinates
-            cells[i].Coordinates = streetPiece.transform.GetChild(2).position + cellCoordinatesDelta;
-        }
-
-        return cells;
     }
 }
